d213079747544432167b4f95b24ef42614f82939
Can Kincal <can.kincal@efectura.com>
2020-01-24 20:37:23 +0300

    Initial Commit

---
+++ SampleModelPassing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleModelPassing
+//
+//  Created by Can Kincal on 24.01.2020.
+//  Copyright © 2020 Can Kincal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
